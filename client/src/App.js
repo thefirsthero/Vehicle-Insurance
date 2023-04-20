b76ac0640132ab28f5868094485dfefdd00760f6
vukosimoyane <vukosi.moyane@sanlam.co.za>
2023-04-20 16:36:48 +0200

    adding faq page

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 import Home from './Component/Home/Home';
 import SignIn from './Component/Signin/Signin';
 import SignUp from './Component/Signup/Signup';
-
+import FAQ from './Component/FAQ/FAQ';
 
 class App extends Component {
   render(){
@@ -19,6 +19,7 @@ class App extends Component {
             <Route exact path="/home" component={Home}></Route>
             <Route exact path="/" component={SignIn}></Route>
             <Route exact path="/register" component={SignUp} ></Route>
+            <Route exact path="/FAQ" component={FAQ} ></Route>
           </Switch>
         </Router>
       </div>
